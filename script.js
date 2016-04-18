6c6ff8a1e88fac7c162bd30db78a70237c1afc04
jzhou94 <jzhou94@bu.edu>
2016-04-18 07:44:47 -0400

    first draf

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-    var academicsLeft = [false, false, false, false, false];
-    var academicsRight = [false, false, false, false, false];
+    var academicsLeft = [false, false, false, false];
+    var academicsRight = [false, false, false, false];
 
     window.onload = start;
 
@@ -7,6 +7,18 @@
     function start() {
       menuBox('Home', 'HomePage');
     }
+
+
+
+    $(function() {
+        $('.nav a').on('click', function(){ 
+            if($('.navbar-toggle').css('display') !='none'){
+                $(".navbar-toggle").trigger( "click" );
+            }
+        });
+    });
+
+
 
     function menuBox(button, page) {
         document.getElementById('Home').style.backgroundColor = "white";
@@ -54,7 +66,6 @@
         var a1 = document.getElementsByClassName("A1");
         var a2 = document.getElementsByClassName("A2");
         var a3 = document.getElementsByClassName("A3");
-        var a4 = document.getElementsByClassName("A4");
 
         for (i = 0; i < tableNum.length; i++) { 
 
@@ -62,29 +73,26 @@
             a1[i].style.display = "none";
             a2[i].style.display = "none";
             a3[i].style.display = "none";
-            a4[i].style.display = "none";
         }
 
         var c0 = document.getElementById("C0");
         var c1 = document.getElementById("C1");
         var c2 = document.getElementById("C2");
         var c3 = document.getElementById("C3");
-        var c4 = document.getElementById("C4");
 
 
-        for (h = 0; h < 5; h++) { 
+        for (h = 0; h < 4; h++) { 
             academicsLeft[h] = false;
         }
 
 
         if (thisBool === false) {
 
-            c0.style.height = "18%";
-            c1.style.height = "18%";
-            c2.style.height = "18%";
-            c3.style.height = "18%";
-            c4.style.height = "18%";
-            classNum.style.height = "28%";
+            c0.style.height = "20%";
+            c1.style.height = "20%";
+            c2.style.height = "20%";
+            c3.style.height = "20%";
+            classNum.style.height = "38%";
 
             academicsLeft[number] = true;
             var j;
@@ -94,11 +102,10 @@
 
         } else {
             academicsLeft[number] = false;
-            c0.style.height = "20%";
-            c1.style.height = "20%";
-            c2.style.height = "20%";
-            c3.style.height = "20%";
-            c4.style.height = "20%";
+            c0.style.height = "24%";
+            c1.style.height = "24%";
+            c2.style.height = "24%";
+            c3.style.height = "24%";
         }
     }
 
@@ -107,47 +114,43 @@
     function displayRight(table, number, c) {
 
         var tableNum = document.getElementsByClassName(table);
-        number = number - 5;
+        number = number - 4;
         var thisBool = academicsRight[number];
         var classNum = document.getElementById(c);
 
         var i;
+        var a4 = document.getElementsByClassName("A4");
         var a5 = document.getElementsByClassName("A5");
         var a6 = document.getElementsByClassName("A6");
         var a7 = document.getElementsByClassName("A7");
-        var a8 = document.getElementsByClassName("A8");
-        var a9 = document.getElementsByClassName("A9");
 
 
         for (i = 0; i < tableNum.length; i++) { 
 
+            a4[i].style.display = "none";
             a5[i].style.display = "none";
             a6[i].style.display = "none";
             a7[i].style.display = "none";
-            a8[i].style.display = "none";
-            a9[i].style.display = "none";
         }
 
+        var c4 = document.getElementById("C4");
         var c5 = document.getElementById("C5");
         var c6 = document.getElementById("C6");
         var c7 = document.getElementById("C7");
-        var c8 = document.getElementById("C8");
-        var c9 = document.getElementById("C9");
 
 
-        for (h = 0; h < 5; h++) { 
+        for (h = 0; h < 4; h++) { 
             academicsRight[h] = false;
         }
 
 
         if (thisBool === false) {
 
-            c5.style.height = "18%";
-            c6.style.height = "18%";
-            c7.style.height = "18%";
-            c8.style.height = "18%";
-            c9.style.height = "18%";
-            classNum.style.height = "28%";
+            c4.style.height = "20%";
+            c5.style.height = "20%";
+            c6.style.height = "20%";
+            c7.style.height = "20%";
+            classNum.style.height = "38%";
 
             academicsRight[number] = true;
             var j;
@@ -155,14 +158,11 @@
                 tableNum[j].style.display = "block";
             }
 
-
-
         } else {
             academicsRight[number] = false;
-            c5.style.height = "20%";
-            c6.style.height = "20%";
-            c7.style.height = "20%";
-            c8.style.height = "20%";
-            c9.style.height = "20%";
+            c4.style.height = "24%";
+            c5.style.height = "24%";
+            c6.style.height = "24%";
+            c7.style.height = "24%";
         }
     }
