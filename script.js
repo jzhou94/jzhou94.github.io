e0c5db07aa28395c1e1320f00539dc2cb2d6641d
jzhou94 <jzhou94@bu.edu>
2016-04-25 15:52:31 -0400

    Site Remake

--- script.js
+++ script.js
@@ -1,57 +1,54 @@
     var academicsLeft = [false, false, false, false];
-    var academicsRight = [false, false, false, false];
 
     window.onload = start;
 
 
     function start() {
-      menuBox('Home', 'HomePage');
+      menuBox('Home', 'HomePage', 1);
+      aboutMe(0);
     }
 
+    function aboutMe(aboutOn) {
+        if (aboutOn == 0) {
+            document.getElementById('homeFill').style.height = "30%";
+            document.getElementById('aboutTag').style.display = "block";
+            document.getElementById('aboutContent').style.display = "none";
+            document.getElementById('downloadTag').style.display = "none";
+        } else {
+            document.getElementById('homeFill').style.height = "0px";
+            document.getElementById('aboutTag').style.display = "none";
+            document.getElementById('aboutContent').style.display = "block";
+            document.getElementById('downloadTag').style.display = "block";
+        }
+    }
 
-
-    $(function() {
-        $('.nav a').on('click', function(){ 
-            if($('.navbar-toggle').css('display') !='none'){
-                $(".navbar-toggle").trigger( "click" );
-            }
-        });
-    });
-
-    $(".menu").on("click", function() {
-        $("body").scrollTop(0);
-    });
-
-
-    function menuBox(button, page) {
+    function menuBox(button, page, change) {
         document.getElementById('Home').style.backgroundColor = "white";
         document.getElementById('Projects').style.backgroundColor = "white";
-        document.getElementById('Academics').style.backgroundColor = "white";
         document.getElementById('Contact').style.backgroundColor = "white";
-
-        document.getElementById('Home').style.fontSize = "85%";
-        document.getElementById('Projects').style.fontSize = "85%";
-        document.getElementById('Academics').style.fontSize = "85%";
-        document.getElementById('Contact').style.fontSize = "85%";
 
         document.getElementById('Home').style.fontWeight = "normal";
         document.getElementById('Projects').style.fontWeight = "normal";
-        document.getElementById('Academics').style.fontWeight = "normal";
         document.getElementById('Contact').style.fontWeight = "normal";
 
-        document.getElementById('Home').style.boxShadow = "4px 4px 2px rgb(220, 220, 220)";
-        document.getElementById('Projects').style.boxShadow = "4px 4px 2px rgb(220, 220, 220)";
-        document.getElementById('Academics').style.boxShadow = "4px 4px 2px rgb(220, 220, 220)";
-        document.getElementById('Contact').style.boxShadow = "4px 4px 2px rgb(220, 220, 220)";
+        var c0 = document.getElementsByClassName("normal");
+        var c1 = document.getElementsByClassName("clicked");
+        for (i = 0; i < 3; i++) {
+            if (i == change) {
+                c0[i].style.display = "none";
+                c1[i].style.display = "block";
+            } else {
+                c0[i].style.display = "block";
+                c1[i].style.display = "none";
+            }
+        }
+
 
         var bStyle = document.getElementById(button);
-        bStyle.style.fontSize = "95%";
         bStyle.style.fontWeight = "bold";
-        bStyle.style.boxShadow = "4px 4px 2px rgb(200, 200, 200)";
 
         document.getElementById('HomePage').style.display = "none";
         document.getElementById('ProjectsPage').style.display = "none";
-        document.getElementById('AcademicsPage').style.display = "none";
         document.getElementById('ContactPage').style.display = "none";
         var display = document.getElementById(page);
         display.style.display = "block";
@@ -91,11 +88,12 @@
 
         if (thisBool === false) {
 
-            c0.style.height = "20%";
-            c1.style.height = "20%";
-            c2.style.height = "20%";
-            c3.style.height = "20%";
-            classNum.style.height = "38%";
+            c0.style.height = "18%";
+            c1.style.height = "18%";
+            c2.style.height = "18%";
+            c3.style.height = "18%";
+            classNum.style.height = "40%";
+            classNum.style.color = "#27aae1";
 
             academicsLeft[number] = true;
             var j;
@@ -109,63 +107,10 @@
             c1.style.height = "24%";
             c2.style.height = "24%";
             c3.style.height = "24%";
-        }
-    }
 
-
-
-    function displayRight(table, number, c) {
-
-        var tableNum = document.getElementsByClassName(table);
-        number = number - 4;
-        var thisBool = academicsRight[number];
-        var classNum = document.getElementById(c);
-
-        var i;
-        var a4 = document.getElementsByClassName("A4");
-        var a5 = document.getElementsByClassName("A5");
-        var a6 = document.getElementsByClassName("A6");
-        var a7 = document.getElementsByClassName("A7");
-
-
-        for (i = 0; i < tableNum.length; i++) { 
-
-            a4[i].style.display = "none";
-            a5[i].style.display = "none";
-            a6[i].style.display = "none";
-            a7[i].style.display = "none";
-        }
-
-        var c4 = document.getElementById("C4");
-        var c5 = document.getElementById("C5");
-        var c6 = document.getElementById("C6");
-        var c7 = document.getElementById("C7");
-
-
-        for (h = 0; h < 4; h++) { 
-            academicsRight[h] = false;
-        }
-
-
-        if (thisBool === false) {
-
-            c4.style.height = "20%";
-            c5.style.height = "20%";
-            c6.style.height = "20%";
-            c7.style.height = "20%";
-            classNum.style.height = "38%";
-
-            academicsRight[number] = true;
-            var j;
-            for (j = 0; j < tableNum.length; j++) {
-                tableNum[j].style.display = "block";
-            }
-
-        } else {
-            academicsRight[number] = false;
-            c4.style.height = "24%";
-            c5.style.height = "24%";
-            c6.style.height = "24%";
-            c7.style.height = "24%";
+            c0.style.color = "black";
+            c1.style.color = "black";
+            c2.style.color = "black";
+            c3.style.color = "black";
         }
     }
